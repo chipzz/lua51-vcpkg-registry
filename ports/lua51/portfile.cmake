faec78f6522efa49321ad2bd28c6c1af750614a4
Jan Van Buggenhout <chipzz@chipzz.be>
2021-06-22 01:05:07 +0200

    Add lua51 port to ports

---
+++ ports/lua51/portfile.cmake
@@ -0,0 +1,64 @@
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://www.lua.org/ftp/lua-5.1.5.tar.gz"
+    FILENAME "lua-5.1.5.tar.gz"
+    SHA512 0142fefcbd13afcd9b201403592aa60620011cc8e8559d4d2db2f92739d18186860989f48caa45830ff4f99bfc7483287fd3ff3a16d4dec928e2767ce4d542a9
+)
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES
+    tools INSTALL_TOOLS
+)
+if(VCPKG_TARGET_IS_IOS AND "tools" IN_LIST FEATURES)
+    message(FATAL_ERROR "lua[tools] is not supported for iOS platform build")
+endif()
+
+set(ENABLE_LUA_CPP 0)
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+#    PREFER_NINJA
+    OPTIONS_RELEASE
+        ${FEATURE_OPTIONS}
+    OPTIONS
+        -DCOMPILE_AS_CPP=OFF
+    OPTIONS_DEBUG
+        -DSKIP_INSTALL_HEADERS=ON
+)
+vcpkg_install_cmake()
+
+if("cpp" IN_LIST FEATURES) # lua[cpp] will create lua-c++, which uses C++ name mangling.
+    set(ENABLE_LUA_CPP 1)
+    vcpkg_configure_cmake(
+        SOURCE_PATH ${SOURCE_PATH}
+#        PREFER_NINJA
+        OPTIONS
+            ${FEATURE_OPTIONS}
+            -DCOMPILE_AS_CPP=ON
+        OPTIONS_DEBUG
+            -DSKIP_INSTALL_HEADERS=ON
+    )
+    vcpkg_install_cmake()
+endif()
+
+vcpkg_copy_pdbs()
+vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/lua)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    if(VCPKG_TARGET_IS_WINDOWS)
+        file(READ ${CURRENT_PACKAGES_DIR}/include/luaconf.h LUA_CONF_H)
+        string(REPLACE "defined(LUA_BUILD_AS_DLL)" "1" LUA_CONF_H "${LUA_CONF_H}")
+        file(WRITE ${CURRENT_PACKAGES_DIR}/include/luaconf.h "${LUA_CONF_H}")
+    endif()
+endif()
+
+# Handle post-build CMake instructions
+configure_file(${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in  ${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake @ONLY)
+file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+
+# Handle copyright
+file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
